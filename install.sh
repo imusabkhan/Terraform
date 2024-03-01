34def1505f3729e0be3795ab7879841034f3dcc3
imusabkhan <65178517+imusabkhan@users.noreply.github.com>
2024-03-02 00:25:45 +0500

    new branch commit

--- install.sh
+++ install.sh
@@ -11,3 +11,9 @@ sudo systemctl start docker
 
 # Add the current user to the Docker group
 sudo usermod -aG docker $(whoami)
+
+#Pull the DVWA image
+sudo docker pull vulnerables/web-dvwa
+
+#Port forward on local to EC2 on Port 80
+sudo docker run -d -p 80:80 vulnerables/web-dvwa
